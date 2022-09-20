47b5ec8227817b9d12c11e0aa0e85310ec767a9b
Lilia Bezrukova <lilia_dyatlova@mail.ru>
2022-09-20 20:53:10 +0400

    Zadacha 038

---
+++ seminar05/Homework/zadacha038/Program.cs
@@ -0,0 +1,39 @@
+﻿/*Задайте массив вещественных чисел. Найдите 
+разницу между максимальным и минимальным элементов массива.*/
+
+//Я еще работаю над ней, надеюсь успею до Вашей проверки))
+
+int size = 5;
+double[] array = FillArray(size);
+PrintArray(FillArray(size));
+PrintArray(FindMaxNum(array));
+
+
+
+double[] FillArray(int size)
+{
+    double[] array = new double [size];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0,100);
+    }
+    return array;
+}
+
+void PrintArray (double[] array)
+{
+    Console.Write($"[{string.Join (", ", array)}]"); 
+    Console.WriteLine();
+}
+
+double FindMaxNum (double[] array)
+{
+    double max = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max);
+        max = array[i];
+    }
+    return max;
+}
